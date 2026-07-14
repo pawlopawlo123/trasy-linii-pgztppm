319e80eb17e122f7b407045e29b516272046f540
pawlopawlo123 <rojewskipawel123@gmail.com>
2026-07-14 12:48:25 +0200

    Add bus line data for P01, P02, and P03

---
+++ linie.js
@@ -0,0 +1,51 @@
+const linie = {
+
+P01:{
+
+    "Cisówka - Pętla":[
+        {n:"Mińsk Mazowiecki /Dw. PKP/ 02"},
+        {n:"Rondo Bitwy Warszawskiej 1920r. 01"},
+        {n:"Kazikowskiego 03"}
+        {n:"Urząd Miasta 03"}
+        {n:"11 Listopada 02"}
+    ],
+
+    "Mińsk Mazowiecki /Dw. PKP/":[
+        {n:"Dworzec"},
+        {n:"Kościół"},
+        {n:"Rynek"}
+    ]
+
+},
+
+P02:{
+
+    "Rynek (Stanisławów)":[
+        {n:"Okuniew 01"},
+        {n:"Jeździecka 01"},
+        {n:"Szosowa 01"},
+        {n:"Rynek (Stanisławów)"}
+    ],
+
+    "Okuniew":[
+        {n:"Rynek (Stanisławów)"},
+        {n:"Szosowa 02"},
+        {n:"Jeździecka 02"},
+        {n:"Okuniew 02"}
+    ]
+
+},
+
+P03:{
+
+  "Michałów - Pętla":[
+    {n:"Okuniew 01"}
+],
+
+  "Okuniew":[
+    {n:"Michałów - Pętla"},
+  ]
+
+},
+
+};
