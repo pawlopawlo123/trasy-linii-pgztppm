3ce23714aadc3a03e5591df479c62b321257c6b7
pawlopawlo123 <rojewskipawel123@gmail.com>
2026-07-14 13:00:57 +0200

    Add menu and route display functions

--- linie.js
+++ linie.js
@@ -49,3 +49,130 @@ P03:{
 }
 
 };
+
+const header = document.getElementById("header");
+const content = document.getElementById("content");
+
+
+function menu(){
+
+    header.innerHTML = "Menu";
+
+    content.innerHTML = `
+
+    <div class="card" onclick="listaLinii()">
+        🚌 Trasy linii
+    </div>
+
+    `;
+
+}
+
+
+
+function listaLinii(){
+
+    header.innerHTML = "Trasy linii";
+
+    content.innerHTML = `
+
+    <div class="back" onclick="menu()">
+        ← Powrót
+    </div>
+
+    `;
+
+
+    for(let linia in linie){
+
+        content.innerHTML += `
+
+        <div class="card" onclick="kierunki('${linia}')">
+            ${linia}
+        </div>
+
+        `;
+
+    }
+
+}
+
+
+
+function kierunki(linia){
+
+    header.innerHTML = linia;
+
+    content.innerHTML = `
+
+    <div class="back" onclick="listaLinii()">
+        ← Powrót
+    </div>
+
+    `;
+
+
+    for(let kierunek in linie[linia]){
+
+        content.innerHTML += `
+
+        <div class="card" onclick="trasa('${linia}','${kierunek}')">
+            ${kierunek}
+        </div>
+
+        `;
+
+    }
+
+}
+
+
+
+function trasa(linia,kierunek){
+
+    header.innerHTML = linia+" → "+kierunek;
+
+
+    content.innerHTML = `
+
+    <div class="back" onclick="kierunki('${linia}')">
+        ← Powrót
+    </div>
+
+    <div class="route">
+
+    </div>
+
+    `;
+
+
+    let route=document.querySelector(".route");
+
+
+    linie[linia][kierunek].forEach(przystanek=>{
+
+
+        route.innerHTML += `
+
+        <div class="stop">
+
+            <div class="circle"></div>
+
+            <div class="name">
+                ${przystanek.n}
+            </div>
+
+        </div>
+
+        `;
+
+
+    });
+
+
+}
+
+
+// start aplikacji
+
+menu();
